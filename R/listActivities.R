c65da31573f485370990ec2d0799a3bf97a10905
derring-do <5824530+derring-do@users.noreply.github.com>
2018-07-12 11:16:25 -0400

    add basic status error handling to listActivities

--- R/listActivities.R
+++ R/listActivities.R
@@ -10,13 +10,22 @@ listActivities <- function() {
   checkRenviron("ADOBE_BEARER_TOKEN")
   checkRenviron("ADOBEIO_API_KEY")
   r <- GET(url = paste0("https://mc.adobe.io/", Sys.getenv("ADOBE_TENANT_NAME"), "/target/activities"),
-           add_headers("Authorization" = paste("Bearer", Sys.getenv("ADOBE_BEARER_TOKEN")),
+           add_headers("Authorization" = Sys.getenv("ADOBE_BEARER_TOKEN"),
                        "Content-Type" = "application/vnd.adobe.target.v1+json",
                        "X-Api-Key" = Sys.getenv("ADOBEIO_API_KEY")
            )
   )
-  
-  activities <- content(r, "parsed", "application/json") 
-  activities <- lapply(activities$activities, data.frame, stringsAsFactors = FALSE) %>% bind_rows %>% arrange(desc(modifiedAt)) 
-  return(activities)
+  r <- GET(url = paste0("https://mc.adobe.io/", Sys.getenv("ADOBE_TENANT_NAME"), "/target/activities"),
+           add_headers("Authorization" = "12345",
+                       "Content-Type" = "application/vnd.adobe.target.v1+json",
+                       "X-Api-Key" = Sys.getenv("ADOBEIO_API_KEY")
+           )
+  )
+  if(r$status_code == 200) {
+    activities <- content(r, "parsed", "application/json") 
+    activities <- lapply(activities$activities, data.frame, stringsAsFactors = FALSE) %>% bind_rows %>% arrange(desc(modifiedAt)) 
+    return(activities) 
+  } else {
+    stop(r)
+  }
 }
