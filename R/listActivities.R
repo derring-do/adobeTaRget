b741dab9399482b5eadde9958ddc1581993dfcc2
derring-do <5824530+derring-do@users.noreply.github.com>
2018-07-16 17:46:49 -0400

    getabperformancereport

--- R/listActivities.R
+++ R/listActivities.R
@@ -23,3 +23,24 @@ listActivities <- function() {
     stop(r)
   }
 }
+
+getAbPerformanceReport <- function(activityId) {
+  checkRenviron("ADOBE_TENANT_NAME")
+  checkRenviron("ADOBEIO_API_KEY")
+  checkRenviron("ADOBEIO_BEARER_TOKEN")
+  r <- httr::GET(url = glue::glue("https://mc.adobe.io/{yourtenantname}/target/activities/ab/{activityId}/report/performance", 
+                                  yourtenantname = Sys.getenv("ADOBE_TENANT_NAME"), 
+                                  activityId = activityId), 
+                 add_headers("Authorization" = Sys.getenv("ADOBEIO_BEARER_TOKEN"),
+                             "Content-Type" = "application/vnd.adobe.target.v1+json",
+                             "X-Api-Key" = Sys.getenv("ADOBEIO_API_KEY")
+                 )
+  )
+  if(r$status_code == 200) {
+    report <- content(r, as="text", type="application/json") %>% jsonlite::fromJSON(simplifyVector = FALSE)
+    return(report) 
+  } else {
+    stop(r)
+  }
+}
+
