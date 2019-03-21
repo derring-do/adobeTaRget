0e083659dd4e7f95e2e26a4615672bc964380635
derring-do <5824530+derring-do@users.noreply.github.com>
2019-03-21 15:24:31 -0400

    add activitybyid

--- R/getAbActivityById.R
+++ R/getAbActivityById.R
@@ -1,19 +1,20 @@
-#' Get Audience by ID
-#' Wrapper for Get Audience by ID API call 
-#' Get the audience definition specified by the provided id.
-#' http://developers.adobetarget.com/api/#list-audiences
-#' @param audienceId 
+#' Get AB Activity by ID
+#' Wrapper for Get AB Activity by ID API call 
+#' Fetch the current definition of an AB activity if it is found as referenced by the id.
+#' http://developers.adobetarget.com/api/#get-ab-activity-by-id
+#'
+#' @param activityId 
 #'
 #' @return json
 #' @export
 #' @import httr dplyr
 #'
 #' @examples getAbActivityById("12345")
-getAudienceById <- function(audienceId) {
+getAbActivityById <- function(activityId) {
   checkRenviron("ADOBE_TENANT_NAME")
   checkRenviron("ADOBEIO_API_KEY")
   checkRenviron("ADOBEIO_BEARER_TOKEN")
-  r <- httr::GET(url = glue::glue("https://mc.adobe.io/{yourtenantname}/target/audiences/{audienceId}", 
+  r <- httr::GET(url = glue::glue("https://mc.adobe.io/{yourtenantname}/target/activities/ab/{activityId}", 
                                   yourtenantname = Sys.getenv("ADOBE_TENANT_NAME"), 
                                   activityId = activityId), 
                  add_headers("authorization" = Sys.getenv("ADOBEIO_BEARER_TOKEN"),
